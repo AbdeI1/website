ad4bd13cf9198a3e8c43d9bd2e70526855afa3b1
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-10-28 23:26:07 -0500

    x

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import {
   Button,
   Container,
+  Divider,
   Link,
   Stack,
   SvgIcon,
@@ -11,13 +12,32 @@ import GitHubIcon from "@mui/icons-material/GitHub";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 
+import { Fira_Code } from "next/font/google";
+
+const fira_code = Fira_Code({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-roboto-mono",
+});
+
 export default function Home() {
   return (
     <Stack spacing={8} className="h-screen justify-center">
-      <Typography variant="h1" className="text-center" color="primary">
+      <Typography
+        variant="h1"
+        className="text-center"
+        color="primary"
+        fontFamily={fira_code.style.fontFamily}
+      >
         Abdelrahman Mokbel
       </Typography>
-      <Stack direction="row" spacing={8} className="justify-center">
+      <Stack
+        direction="row"
+        spacing={2}
+        className="justify-center"
+        divider={<Divider orientation="vertical" variant="middle" flexItem />}
+      >
         {[
           { Icon: GitHubIcon, link: "https://google.com" },
           { Icon: LinkedInIcon, link: "https://google.com" },
@@ -28,7 +48,7 @@ export default function Home() {
             target="_blank"
             href={link}
             rel="noopener"
-            variant="contained"
+            variant="text"
           >
             <Icon sx={{ fontSize: 72 }} />
           </Button>
