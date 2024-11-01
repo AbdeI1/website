de5392a4498038582a3c4018f33ad10d18cb56b3
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-11-01 18:35:13 -0500

    moved stuff around

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useState } from "react";
 
 import {
+  Box,
   Button,
   Container,
   Divider,
@@ -17,6 +18,7 @@ import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import EmailIcon from "@mui/icons-material/Email";
 
 import { Fira_Code } from "next/font/google";
+import Script from "next/script";
 
 const fira_code = Fira_Code({
   weight: "400",
@@ -25,33 +27,90 @@ const fira_code = Fira_Code({
   variable: "--font-roboto-mono",
 });
 
-const TypeWriter : typeof Typography = ({ ... props }) => {
+const TypeWriter: typeof Typography = ({ ...props }) => {
   const text = props.children as string;
-  const [index, setIndex] = useState(0);
+
+  const [index, setIndex] = useState({ i: 0, typo: "" });
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      setIndex((prevIndex) => {
-        if (prevIndex === text.length - 1) {
-          clearInterval(interval);
+    let timeout: NodeJS.Timeout;
+    const characters = new Set("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    const type = () => {
+      clearTimeout(timeout);
+      setIndex(({ i, typo }) => {
+        if (i < text.length) {
+          if (typo !== "") {
+            timeout = setTimeout(type, Math.random() * 50 + 190);
+            typo = "";
+          } else if (i > 0 && typo === "" && Math.random() < 0.1) {
+            timeout = setTimeout(type, Math.random() * 100 + 380);
+            let char = Array.from(characters.difference(new Set(text[i])))[
+              Math.floor(Math.random() * 25)
+            ];
+            if (text[i] === text[i].toLowerCase()) char = char.toLowerCase();
+            typo = char;
+          } else {
+            timeout = setTimeout(type, Math.random() * 50 + 220);
+            i = i + 1;
+          }
         }
-        return prevIndex + 1;
+        return { i, typo };
       });
-    }, Math.random() * 100 + 100);
-    return () => clearInterval(interval);
-  }, [ text ]);
+    };
+    type();
+    return () => clearTimeout(timeout);
+  }, [text]);
 
-  return <Typography { ... props }>{text.slice(0, index)}</Typography>;
-}
+  return (
+    <Typography className="flex justify-center" {...props}>
+      {text.slice(0, index.i) + index.typo}
+      <Box
+        sx={{
+          marginLeft: 2,
+          width: "0.1em",
+          height: "1em",
+          bgcolor: "primary.main",
+          "@keyframes blink": {
+            from: { opacity: 0 },
+            "50%": { opacity: 1 },
+          },
+          animation: "blink 500ms infinite step-end",
+        }}
+      />
+    </Typography>
+  );
+};
+
+const Grain = () => {
+  return (
+    <>
+      <canvas
+        id="noise"
+        style={{
+          zIndex: 100,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+          pointerEvents: "none",
+          opacity: 0.1,
+        }}
+      />
+      <Script src="/test/noise.js" />
+    </>
+  );
+};
 
 export default function Home() {
   return (
     <Stack spacing={8} className="h-screen justify-center">
-      <TypeWriter 
+      <Grain />
+      <TypeWriter
         variant="h1"
-        className="text-center"
         color="primary"
-        fontFamily={fira_code.style.fontFamily}>
+        fontFamily={fira_code.style.fontFamily}
+      >
         Abdelrahman Mokbel
       </TypeWriter>
       <Stack
