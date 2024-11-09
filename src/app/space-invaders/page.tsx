f939ec4348945737811835fb82b1c66861d0c490
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-11-08 21:53:07 -0600

    cube

--- src/app/space-invaders/page.tsx
+++ src/app/space-invaders/page.tsx
@@ -1,10 +1,22 @@
-import Script from "next/script";
+"use client";
+import { Canvas } from "@react-three/fiber";
 
 export default function SpaceInvaders() {
   return (
-    <>
-      <Script type="module" src="./index.js"></Script>
-      <canvas id="gfxCanvas"></canvas>
-    </>
+    <Canvas style={{ height: "100vh", width: "100vw", background: "black" }}>
+      <ambientLight intensity={Math.PI / 2} />
+      <spotLight
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+        decay={0}
+        intensity={Math.PI}
+      />
+      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
+      <mesh>
+        <boxGeometry args={[1, 1, 1]} />
+        <meshStandardMaterial color={"hotpink"} />
+      </mesh>
+    </Canvas>
   );
 }
