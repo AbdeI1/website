c136f0cbecfe5cd258d7da3209a50c5a3737073f
AbdeI1 <abdelrahmanmmokbel@gmail.com>
2024-11-10 01:09:52 -0600

    bloom pipeline

--- src/app/space-invaders/page.tsx
+++ src/app/space-invaders/page.tsx
@@ -1,21 +1,112 @@
 "use client";
-import { Canvas } from "@react-three/fiber";
+import { Canvas, useFrame, useThree } from "@react-three/fiber";
+import {
+  Layers,
+  Material,
+  Mesh,
+  MeshBasicMaterial,
+  ShaderMaterial,
+  Vector2,
+} from "three";
+import {
+  EffectComposer,
+  OutputPass,
+  RenderPass,
+  ShaderPass,
+  UnrealBloomPass,
+} from "three/examples/jsm/Addons.js";
+
+const BloomPipeline = () => {
+  const { gl: renderer, scene, camera } = useThree();
+
+  const bloomLayer = new Layers();
+  bloomLayer.set(1);
+
+  const renderScene = new RenderPass(scene, camera);
+
+  const bloomPass = new UnrealBloomPass(
+    new Vector2(window.innerWidth, window.innerHeight),
+    0.3,
+    0.1,
+    0.2
+  );
+
+  const bloomComposer = new EffectComposer(renderer);
+  bloomComposer.renderToScreen = false;
+  bloomComposer.addPass(renderScene);
+  bloomComposer.addPass(bloomPass);
+
+  const mixPass = new ShaderPass(
+    new ShaderMaterial({
+      uniforms: {
+        baseTexture: { value: null },
+        bloomTexture: { value: bloomComposer.renderTarget2.texture },
+      },
+      vertexShader: `
+        varying vec2 vUv;
+        void main() {
+          vUv = uv;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+        }
+      `,
+      fragmentShader: `
+        uniform sampler2D baseTexture;
+        uniform sampler2D bloomTexture;
+        varying vec2 vUv;
+        void main() {
+          gl_FragColor = ( texture2D( baseTexture, vUv ) + vec4( 1.0 ) * texture2D( bloomTexture, vUv ) );
+        }
+      `,
+      defines: {},
+    }),
+    "baseTexture"
+  );
+  mixPass.needsSwap = true;
+
+  const outputPass = new OutputPass();
+
+  const finalComposer = new EffectComposer(renderer);
+  finalComposer.addPass(renderScene);
+  finalComposer.addPass(mixPass);
+  finalComposer.addPass(outputPass);
+
+  const transparentMaterial = new MeshBasicMaterial({
+    opacity: 0,
+    transparent: true,
+  });
+
+  useFrame(({ scene }) => {
+    const materials: { [key: string]: Material | Material[] } = {};
+    scene.traverse((obj) => {
+      const mesh = obj as Mesh;
+      if (mesh.isMesh && !bloomLayer.test(mesh.layers)) {
+        materials[mesh.uuid] = mesh.material;
+        mesh.material = transparentMaterial;
+      }
+    });
+    bloomComposer.render();
+    scene.traverse((obj) => {
+      const mesh = obj as Mesh;
+      if (materials[mesh.uuid]) {
+        mesh.material = materials[mesh.uuid];
+      }
+    });
+    finalComposer.render();
+  }, 1);
+
+  return <></>;
+};
 
 export default function SpaceInvaders() {
+  const bloomLayer = new Layers();
+  bloomLayer.enable(1);
   return (
     <Canvas style={{ height: "100vh", width: "100vw", background: "black" }}>
-      <ambientLight intensity={Math.PI / 2} />
-      <spotLight
-        position={[10, 10, 10]}
-        angle={0.15}
-        penumbra={1}
-        decay={0}
-        intensity={Math.PI}
-      />
-      <pointLight position={[-10, -10, -10]} decay={0} intensity={Math.PI} />
-      <mesh>
-        <boxGeometry args={[1, 1, 1]} />
-        <meshStandardMaterial color={"hotpink"} />
+      <ambientLight intensity={5} />
+      <BloomPipeline />
+      <mesh layers={bloomLayer} scale={0.5}>
+        <icosahedronGeometry args={[1, 15]} />
+        <meshBasicMaterial color="red" />
       </mesh>
     </Canvas>
   );
